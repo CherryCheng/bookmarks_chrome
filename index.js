5064e4d6aecda8c6191ae3fda44e26ef246b4879
sunchuanjun <sunchuanjun@sm.vvip-u.com>
2018-06-08 12:53:22 +0800

    add

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ let html = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="./s.png" type="image/x-icon"/>
     <style>
         body {
             padding: 20px
